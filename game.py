6e4d8a18e3d8124a67bcc738305edd49c0f9370c
nikitos525e34 <n.fokin.bmw@gmail.com>
2025-05-03 13:54:47 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 from pygame import *
-win_width = 700
-win_height = 500
+win_width = 900
+win_height = 700
+wall_size = 50
 window = display.set_mode((win_width, win_height))
 display.set_caption("гра у квача")
-bg = transform.scale(image.load("1-56.jpg"),(win_width, win_height))
+bg = transform.scale(image.load("1-56.jpg"), (win_width, win_height))
 
-x1 = 100
-y1 = 300
+x1 = 50
+y1 = 100
 
-x2 = 300
-y2 = 300
+x2 = win_width - 150
+y2 = win_height - 100
 
 
 class GameSprite(sprite.Sprite):
@@ -39,9 +40,80 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 
+class Wall(GameSprite):
+    def __init__(self, wall_img, player_x, player_y):
+        super().__init__(wall_img, player_x, player_y, wall_size, wall_size, 0)
 
-sprite1 = Player("T64_5.gif", 100, 100, 120, 50, 5)
-sprite2 = Player("tank2.png", 200, 100, 120, 50, 5)
+    def stop_tank(self):
+        pass
+
+
+class StoneWall(Wall):
+    def __init__(self, player_x, player_y):
+        super().__init__("stone-wall.jpg", player_x, player_y)
+
+
+class BrickWall(Wall):
+    def __init__(self, player_x, player_y):
+        super().__init__("brick-wall.png", player_x, player_y)
+
+
+sprite1 = Player("T64_5.gif", x1, y1, 120, 50, 5)
+sprite2 = Player("tank2.png", x2, y2, 120, 50, 5)
+
+# код для створення стінок
+# створення рамки
+stone_walls = sprite.Group()
+width_stone_walls = win_width // wall_size
+height_stone_walls = win_height // wall_size
+
+x = 0
+y = 0
+for i in range(width_stone_walls):
+    stone_wall = StoneWall(x, y)
+    stone_walls.add(stone_wall)
+    x += wall_size
+
+x = 0
+y = win_height - wall_size
+for i in range(width_stone_walls):
+    stone_wall = StoneWall(x, y)
+    stone_walls.add(stone_wall)
+    x += wall_size
+
+x = 0
+y = 0
+for i in range(width_stone_walls):
+    stone_wall = StoneWall(x, y)
+    stone_walls.add(stone_wall)
+    y += wall_size
+
+x = win_width - wall_size
+y = 0
+for i in range(width_stone_walls):
+    stone_wall = StoneWall(x, y)
+    stone_walls.add(stone_wall)
+    y += wall_size
+
+# створення рандомних кам'яних стін перешкод
+stone_wall = StoneWall(350, 150)
+stone_walls.add(stone_wall)
+
+
+# створення цегляних перешкод
+brick_walls = sprite.Group()
+brick_wall = BrickWall(50, 150)
+brick_walls.add(brick_wall)
+brick_wall = BrickWall(100, 150)
+brick_walls.add(brick_wall)
+brick_wall = BrickWall(150, 150)
+brick_walls.add(brick_wall)
+brick_wall = BrickWall(200, 150)
+brick_walls.add(brick_wall)
+
+x
+
+
 
 run = True
 clock = time.Clock()
@@ -53,8 +125,11 @@ while run:
     sprite1.reset()
     sprite2.reset()
 
-    sprite1.update(K_UP, K_DOWN, K_LEFT, K_RIGHT)
-    sprite2.update(K_w, K_s, K_a, K_d)
+    sprite1.update(K_w, K_s, K_a, K_d)
+    sprite2.update(K_UP, K_DOWN, K_LEFT, K_RIGHT)
+
+    stone_walls.draw(window)
+    brick_walls.draw(window)
 
     for e in event.get():
         if e.type == QUIT:
